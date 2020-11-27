409de2f3269800e25fab912f899df45691e642d6
Gustaf Bagus Alamsyah <gustafkonamic@gmail.com>
2020-11-27 14:59:13 +0700

    cloningapple

---
+++ src/component/Home.js
@@ -0,0 +1,168 @@
+import { Component } from 'react';
+
+import AppleIcon from '@material-ui/icons/Apple';
+
+class Home extends Component{
+    render(){
+        return(
+          
+          <div style={{ backgroundImage:"url(https://cdn0-production-images-kly.akamaized.net/buSH1BDc5dQGgeU5x10janRoElE=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3266677/original/053767400_1602614983-iPhone_12_Pro_Max.jpg)", backgroundSize:"cover"} } > 
+  <div class="navbar-fixed">
+  <nav>
+    <div class="nav-wrapper black">
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+      <li><a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<AppleIcon/></a></li>
+        <li><a  href="https://www.apple.com/mac/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mac&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+        <li><a href="https://www.apple.com/ipad/">iPad&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+        <li><a href="https://www.apple.com/iphone/">iPhone&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+        <li><a  href="https://www.apple.com/watch/">Watch&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+        <li><a  href="https://www.apple.com/tv/">TV&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+        <li><a  href="https://www.apple.com/music/">Music&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+        <li><a  href="https://support.apple.com/">Support&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+        <li><a href="sass.html"><i class="material-icons">search</i></a></li>
+      </ul>
+    </div>
+  </nav>
+  </div>
+  <img class="materialboxed" data-caption="A picture of a way with a group of trees in a park" width="1280"  height="500"src="https://timlo.net/wp-content/uploads/2020/10/iphone-12-dan-12-pro-muncul-di-antutu-skor-mengecewakan.jpg"/>
+  <br/>
+  <h1 align="center">iPhone 12</h1>
+  <h4 align="center">Blast Past Fast.</h4>
+  <center>
+    
+  <p>From $29.12/mo. or $699 before trade-in.</p>
+  <p>Buy Directly From Apple With Special Carrier Offers.</p>
+ 
+  </center>
+  <footer class="page-footer #eceff1 blue-grey lighten-5 ">
+          <div class="container">
+            <div class="row">
+              <div class="col 12 grey-text" style={{ fontSize:'small' }}>
+                <p>1. Monthly pricing: Available to qualified customers and requires 0% APR, 24-month installment loan with Citizens One or Apple Card Monthly Installments and iPhone activation with AT&T, Sprint, T-Mobile, or Verizon for purchases in an Apple Store. Taxes and shipping not included. Additional Apple Card Monthly Installments and iPhone Payments terms apply.</p>
+                <p>2. Price includes a $30 AT&T, T-Mobile, Sprint, or VerizonAppleOfficial Apple SupportApple support is here to help. Learn more about popular topics and find resources that will help you with all of your Apple products.discount. Requires activation with carrier. Monthly pricing: Available to qualified customers and requires 0% APR, 24-month installment loan with Citizens One or Apple Card Monthly Installments and iPhone activation with AT&T, Sprint, T-Mobile, or Verizon for purchases in an Apple Store. Taxes and shipping not included. Additional Apple Card Monthly Installments and iPhone Payments terms apply.</p>
+                <p>3. Apple Fitness+ is coming late 2020.</p>
+                <p>4. The Apple One free trial includes only services that you are not currently using through a free trial or a subscription. Plan automatically renews after trial until cancelled. Restrictions and other terms apply.</p>
+                <p>5. New subscribers only. $9.99/month after free trial. No commitment. Plan automatically renews after trial until cancelled.</p>
+                <p>To access and use all the features of Apple Card, you must add Apple Card to Wallet on an iPhone or iPad with iOS 12.4 or later or iPadOS. To manage Apple Card Monthly Installments, you need an iPhone with iOS 13.2 or later or an iPad with iPadOS 13.2 or later. Update to the latest version of iOS or iPadOS by going to Settings &gt; General &gt; Software Update. Tap Download and Install.</p>
+                <p>Available for qualifying applicants in the United States.</p>
+                <p>Apple Card is issued by Goldman Sachs Bank USA, Salt Lake City Branch.</p>
+                <p>Apple TV+ is $4.99/month after free trial. One subscription per Family Sharing group. Offer good for 3 months after eligible device activation. Plan automatically renews until cancelled. Restrictions and other terms apply.</p>
+              </div>
+              <div class="col l12 s12" style={{ fontSize:'small' }}><hr/>
+              <div class="col l2 s12" style={{ fontSize:'small' }}>
+                <p class="black-text">Shop and Learn</p>
+                <ul>
+                  <li><a class="grey-text" href="#">Mac</a></li>
+                  <li><a class="grey-text" href="#">iPad</a></li>
+                  <li><a class="grey-text" href="#">iPhone</a></li>
+                  <li><a class="grey-text" href="#">Watch</a></li>
+                  <li><a class="grey-text" href="#">TV</a></li>
+                  <li><a class="grey-text" href="#">Music</a></li>
+                  <li><a class="grey-text" href="#">AirPods </a></li>
+                  <li><a class="grey-text" href="#">HomePod</a></li>
+                  <li><a class="grey-text" href="#">iPod Touch</a></li>
+                  <li><a class="grey-text" href="#">Accesories</a></li>
+                  <li><a class="grey-text" href="#">Gift Cards</a></li>
+                </ul>
+               </div> 
+
+               <div class="col l3 s12" style={{ fontSize:'small' }}>
+               <p class="black-text">Services</p>
+                <ul>
+                  <li><a class="grey-text" href="#">Apple Music</a></li>
+                  <li><a class="grey-text" href="#">Apple TV+</a></li>
+                  <li><a class="grey-text" href="#">Apple Fitness+</a></li>
+                  <li><a class="grey-text" href="#">Apple News+</a></li>
+                  <li><a class="grey-text" href="#">Apple Arcade</a></li>
+                  <li><a class="grey-text" href="#">iCloud</a></li>
+                  <li><a class="grey-text" href="#">Apple One </a></li>
+                  <li><a class="grey-text" href="#">Apple Card</a></li>
+                  <li><a class="grey-text" href="#">Apple Books</a></li>
+                  <li><a class="grey-text" href="#">App Store</a></li>
+                </ul>
+               <p class="black-text">Account</p>
+               <ul>
+                  <li><a class="grey-text" href="#">Manage Your Apple ID</a></li>
+                  <li><a class="grey-text" href="#">Apple Store Account</a></li>
+                  <li><a class="grey-text" href="#">iCloud.com</a></li>
+                </ul>
+               </div> 
+
+               <div class="col l2 s12" style={{ fontSize:'small' }}>
+               <p class="black-text">Apple Store</p>
+<ul>
+                  <li><a class="grey-text" href="#">Find a Store</a></li>
+                  <li><a class="grey-text" href="#">Shop Online</a></li>
+                  <li><a class="grey-text" href="#">Genius Bar</a></li>
+                  <li><a class="grey-text" href="#">Today at Apple</a></li>
+                  <li><a class="grey-text" href="#">Apple Camp</a></li>
+                  <li><a class="grey-text" href="#">Apple Store App</a></li>
+                  <li><a class="grey-text" href="#">Refurbished and Clearance</a></li>
+                  <li><a class="grey-text" href="#">Financing</a></li>
+                  <li><a class="grey-text" href="#">Apple Trade In</a></li>
+                  <li><a class="grey-text" href="#">Order Status</a></li>
+                  <li><a class="grey-text" href="#">Shopping Help</a></li>
+                </ul>
+                </div> 
+
+               <div class="col l3 s12" style={{ fontSize:'small' }}>
+               <p class="black-text">For Business</p>
+                <ul>
+                  <li><a class="grey-text" href="#">Apple and Business</a></li>
+                  <li><a class="grey-text" href="#">Shop for Business</a></li>
+                </ul>
+               <p class="black-text">For Education</p>
+               <ul>
+                  <li><a class="grey-text" href="#">Apple and Education</a></li>
+                  <li><a class="grey-text" href="#">Shop for K-12</a></li>
+                  <li><a class="grey-text" href="#">Shop for College</a></li>
+                </ul>
+                <p class="black-text">For Healthcare</p>
+               <ul>
+                  <li><a class="grey-text" href="#">Apple in Healthcare</a></li>
+                  <li><a class="grey-text" href="#">Health on Apple Watch</a></li>
+                  <li><a class="grey-text" href="#">Health Records on iPhone</a></li>
+                </ul>
+                <p class="black-text">For Government</p>
+               <ul>
+                  <li><a class="grey-text" href="#">Shop for Government</a></li>
+                  <li><a class="grey-text" href="#">Shop for Veterans and Military</a></li>
+                </ul>
+               </div> 
+
+               <div class="col l2 s12" style={{ fontSize:'small' }}>
+               <p class="black-text">Apple Values</p>
+                <ul>
+                  <li><a class="grey-text" href="#">Accessibility</a></li>
+                  <li><a class="grey-text" href="#">Education</a></li>
+                  <li><a class="grey-text" href="#">Environment</a></li>
+                  <li><a class="grey-text" href="#">Inclusion and Diversity</a></li>
+                  <li><a class="grey-text" href="#">Privacy</a></li>
+                  <li><a class="grey-text" href="#">Supplier Responsibility</a></li>
+                </ul>
+               <p class="black-text">About Apple</p>
+               <ul>
+                  <li><a class="grey-text" href="#">Newsroom</a></li>
+                  <li><a class="grey-text" href="#">Apple Leadership</a></li>
+                  <li><a class="grey-text" href="#">Job Opportunities</a></li>
+                  <li><a class="grey-text" href="#">Investors</a></li>
+                  <li><a class="grey-text" href="#">Events</a></li>
+                  <li><a class="grey-text" href="#">Contact Apple</a></li>
+                </ul>
+               </div>
+              </div>
+              <div class="col l12 s12 grey-text" style={{ fontSize:'small' }}>
+                  <p>More ways to shop: <a href='#'>Find an Apple Store</a> or <a href="#">other retailer</a> near you. Or call 1-800-MY-APPLE.</p><hr/>                 
+              </div>
+              <div class="col l12 s12 grey-text" style={{ fontSize:'small' }}>
+                  <p>Copyright :copyright: 2020 Apple Inc. All rights reserved. &nbsp;&nbsp; <a href="#"><font color="black">Privacy Policy</font></a>&nbsp;&nbsp; |&nbsp;&nbsp; <a href="#"><font color="black">Terms Of Use</font></a>&nbsp;&nbsp;| &nbsp;&nbsp;<a href="#"><font color="black">Sales And Refuns</font></a> &nbsp;&nbsp;|&nbsp;&nbsp; <a href="#"><font color="black">Legal</font></a>&nbsp;|&nbsp;<a href="#"><font color="black">Site Map</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#"><font color="black">United States</font></a></a></p>
+              </div>
+            </div>
+          </div>
+        </footer>
+            </div>
+           )
+        }
+      }
+                      
+export default Home;            
